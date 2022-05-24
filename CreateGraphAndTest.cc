9b8ecb5e3444ae31215b0f27cf5ee1e3936fbcd8
Sammyajit Kapuria <sammyajit.kapuria87@myhunter.cuny.edu>
2022-05-24 19:21:20 -0400

    pt1

--- CreateGraphAndTest.cc
+++ CreateGraphAndTest.cc
@@ -1,16 +1,97 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <fstream>
 #include "graph.h"
 
 using namespace std;
 
 int graphTestDriver(int argc, char **argv) {
 
-    //TODO: Begin your code here. Feel free to add any helper functions or classes you need,
+    // TODO: Begin your code here. Feel free to add any helper functions or classes you need,
     // as long as we only have to call this function to run the specified assignment.
+    string FILENAME = argv[1];
+    ifstream FILE(FILENAME);
+    if(!FILE) {
+        cout<<"BAD FILE!";
+        return 0;
+    }
+    
+    int num_vertices;
+    FILE>>num_vertices;
+    
+    Graph G(num_vertices);
 
+
+  
+         FILE.ignore(1000,'\n');   
+
+    while(!FILE.eof()) {
+          string help;    
+     getline(FILE,help);
+     
+     vector<string> V;
+     for(int i=0;i<help.length();i++) {
+         
+         string s;
+         while(help[i] != ' ' && i < help.length()) {
+             
+             s.push_back(help[i]);
+             
+             
+             i++;
+         }
+         
+         V.push_back(s);
+
+     }
+
+         int num = stoi(V[0]);
+       
+         for(int i=1;i<V.size();i++) {
+             
+             int vert1 = stoi(V[i]);
+             i++;
+             double weight1 = stod(V[i]);
+             Edge E(vert1,weight1);
+           G.insert(num,E);
+             
+                 // insert it together with the vertex
+             
+         }
+        
+    }
+    
+    
+    FILE.close();
+    string FILENAME2 = argv[2];
+    ifstream FILE2(FILENAME2);
+
+    if(!FILE2) {
+        cout<<"COULD NOT OPEN FILE2";
+        return 0;
+    }
+    
+    while(!FILE2.eof()) {
+        
+        int num1;
+        int num2;
+        FILE2>>num1;
+        FILE2>>num2;
+        
+        if(G.connected(num1,num2) == -1) {
+            
+            cout<<num1<<" "<<num2<<": "<<"not_connected"<<endl;
+            
+        }
+        else {
+            
+            cout<<num1<<" "<<num2<<": "<<"connected "<<G.connected(num1,num2)<<endl;
+        }
+        
+        
+    }
+        
     return 0;
 }
 
--- graph.h
+++ graph.h
@@ -11,11 +11,91 @@
 
 using namespace std;
 
-class Grapth{
+class Edge {
+  private:
+    int e;
+    double weight;
+    
+    
+  public:
+  
+    Edge(int e1, double w1) {
+        this->e = e1;
+        this->weight = w1;
+    }
+    Edge() {
+        this->e = 0;
+        this->weight = 0.0;
+    }
+    
+    
+    Edge(const Edge& Edg) {
+        
+        this->e = Edg.e;
+        this->weight = Edg.weight;
+        
+    }
+    
+        Edge& operator=(const Edge& Edg) {
+            
+         if(this!= &Edg) {
+              this->e = Edg.e;
+        this->weight = Edg.weight;
+         }
+         
+         return *this;
+            
+        }
 
+    
+    int get_e() {
+        return e;
+    }
+    
+    double get_w() {
+        return weight;
+    }
+    
+   
 };
 
-
-
+class Graph {
+  
+  private:
+  
+  int number_of_vertices;
+  
+  vector<vector<Edge>> vertex;
+  
+  
+  
+  public:
+    
+  
+  Graph(int n_vertices) {
+      
+      this->number_of_vertices = n_vertices;
+      this->vertex.resize(n_vertices+1);
+      
+      
+  }
+  
+  void insert(int V, Edge E) {
+       vertex[V].push_back(E);
+  }
+    
+  double connected(int vertex1, int vertex2) {
+      
+      for(int i=0;i<vertex[vertex1].size();i++) { 
+          
+          if(vertex[vertex1][i].get_e() == vertex2) { 
+            return vertex[vertex1][i].get_w();
+              
+          }
+      }
+      
+      return -1;  
+  } 
+};
 
 #endif
