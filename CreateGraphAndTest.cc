1853bead0158d03a3dfc0a1d16a6ebabfb4c8738
Sammyajit Kapuria <sammyajit.kapuria87@myhunter.cuny.edu>
2022-05-24 19:45:23 -0400

    pt 1

--- CreateGraphAndTest.cc
+++ CreateGraphAndTest.cc
@@ -10,51 +10,52 @@ int graphTestDriver(int argc, char **argv) {
 
     // TODO: Begin your code here. Feel free to add any helper functions or classes you need,
     // as long as we only have to call this function to run the specified assignment.
-    string FILENAME = argv[1];
+    string FILENAME = argv[1]; // filename is argv[1] string 
     ifstream FILE(FILENAME);
-    if(!FILE) {
+    if(!FILE) { // if file is bad abort 
         cout<<"BAD FILE!";
         return 0;
     }
     
     int num_vertices;
-    FILE>>num_vertices;
+    FILE>>num_vertices; // read num of vertices 
     
-    Graph G(num_vertices);
+    Graph G(num_vertices); // create a graph with a constructor of num_vertices
 
 
   
-         FILE.ignore(1000,'\n');   
+         FILE.ignore(1000,'\n');    // ignore \n because we will be reading strings 
 
-    while(!FILE.eof()) {
-          string help;    
-     getline(FILE,help);
+    while(!FILE.eof()) { // loop as long as file is not at the end 
+          string help;    // new string 
+     getline(FILE,help); // read a line 
      
-     vector<string> V;
-     for(int i=0;i<help.length();i++) {
+     vector<string> V; // create a vector of strings 
+     for(int i=0;i<help.length();i++) { // go through a vector 
          
          string s;
-         while(help[i] != ' ' && i < help.length()) {
+         while(help[i] != ' ' && i < help.length()) { // as long as it is not whitespace or end 
              
-             s.push_back(help[i]);
+             s.push_back(help[i]); // add it to a string 
              
              
              i++;
          }
          
-         V.push_back(s);
+         V.push_back(s); // string in the end is a number as a string so we push it back to V 
 
      }
 
-         int num = stoi(V[0]);
+         int num = stoi(V[0]); // convert first number from a string to int 
        
-         for(int i=1;i<V.size();i++) {
+         for(int i=1;i<V.size();i++) { // go from 1 to the end of vector of strings 
              
-             int vert1 = stoi(V[i]);
+             int vert1 = stoi(V[i]); // convert incoming number from a string to number 
              i++;
-             double weight1 = stod(V[i]);
-             Edge E(vert1,weight1);
-           G.insert(num,E);
+             double weight1 = stod(V[i]); //convert weight to a double 
+             Edge E(vert1,weight1); // create edge 
+             
+           G.insert(num,E); // insert to a graph 
              
                  // insert it together with the vertex
              
@@ -64,8 +65,8 @@ int graphTestDriver(int argc, char **argv) {
     
     
     FILE.close();
-    string FILENAME2 = argv[2];
-    ifstream FILE2(FILENAME2);
+    string FILENAME2 = argv[2]; // a new file 
+    ifstream FILE2(FILENAME2); // read a file2 
 
     if(!FILE2) {
         cout<<"COULD NOT OPEN FILE2";
@@ -76,17 +77,17 @@ int graphTestDriver(int argc, char **argv) {
         
         int num1;
         int num2;
-        FILE2>>num1;
+        FILE2>>num1;// read two numbers from adjacency queries 
         FILE2>>num2;
         
-        if(G.connected(num1,num2) == -1) {
+        if(G.connected(num1,num2) == -1) { // if we get -1 result it is not connected 
             
             cout<<num1<<" "<<num2<<": "<<"not_connected"<<endl;
             
         }
         else {
             
-            cout<<num1<<" "<<num2<<": "<<"connected "<<G.connected(num1,num2)<<endl;
+            cout<<num1<<" "<<num2<<": "<<"connected "<<G.connected(num1,num2)<<endl; // otherwise we print it 
         }
         
         
--- graph.h
+++ graph.h
@@ -13,30 +13,30 @@ using namespace std;
 
 class Edge {
   private:
-    int e;
+      int e; // two private attributes 
     double weight;
     
     
   public:
   
-    Edge(int e1, double w1) {
+    Edge(int e1, double w1) { // regular constructor 
         this->e = e1;
         this->weight = w1;
     }
     Edge() {
-        this->e = 0;
+        this->e = 0; // default constructor setting up values to 0 
         this->weight = 0.0;
     }
     
     
-    Edge(const Edge& Edg) {
+    Edge(const Edge& Edg) { // copy constructor 
         
         this->e = Edg.e;
         this->weight = Edg.weight;
         
     }
     
-        Edge& operator=(const Edge& Edg) {
+        Edge& operator=(const Edge& Edg) { // assignment operator 
             
          if(this!= &Edg) {
               this->e = Edg.e;
@@ -48,15 +48,16 @@ class Edge {
         }
 
     
-    int get_e() {
+    int get_e() { // getter function to get int 
         return e;
     }
     
-    double get_w() {
+    double get_w() { //getter function to get double 
         return weight;
     }
     
    
+
 };
 
 class Graph {
@@ -64,7 +65,7 @@ class Graph {
   private:
   
   int number_of_vertices;
-  
+  // private attributes number_of_vertices and vector of vectors that contain edges 
   vector<vector<Edge>> vertex;
   
   
@@ -75,27 +76,39 @@ class Graph {
   Graph(int n_vertices) {
       
       this->number_of_vertices = n_vertices;
-      this->vertex.resize(n_vertices+1);
+      this->vertex.resize(n_vertices+1); // regular constructor that sets up private attributes 
       
       
   }
   
   void insert(int V, Edge E) {
-       vertex[V].push_back(E);
+       vertex[V].push_back(E); // we push back edge to a vector with a given VERTEXF 
   }
     
   double connected(int vertex1, int vertex2) {
-      
-      for(int i=0;i<vertex[vertex1].size();i++) { 
+      // we check here whether two of the vertex are connected 
+      for(int i=0;i<vertex[vertex1].size();i++) {
           
-          if(vertex[vertex1][i].get_e() == vertex2) { 
-            return vertex[vertex1][i].get_w();
+          
+          if(vertex[vertex1][i].get_e() == vertex2) { // if we found it is equal to vertex2 then it is connected 
+              
+              return vertex[vertex1][i].get_w(); // return weight  
               
           }
+          
+          
       }
       
-      return -1;  
+      return -1; // otherwise return -1 
+      
   } 
+    
 };
+
+
+
+
+
+
 
 #endif
