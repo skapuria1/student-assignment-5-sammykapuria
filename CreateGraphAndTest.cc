5fa933f3fff657e743d11b61944174e6e7db4e50
Sammyajit Kapuria <sammyajit.kapuria87@myhunter.cuny.edu>
2022-05-24 20:11:17 -0400

    one

--- CreateGraphAndTest.cc
+++ CreateGraphAndTest.cc
@@ -8,72 +8,70 @@ using namespace std;
 
 int graphTestDriver(int argc, char **argv) {
 
-    // TODO: Begin your code here. Feel free to add any helper functions or classes you need,
-    // as long as we only have to call this function to run the specified assignment.
-    string FILENAME = argv[1]; // filename is argv[1] string 
-    ifstream FILE(FILENAME);
-    if(!FILE) { // if file is bad abort 
-        cout<<"BAD FILE!";
-        return 0;
-    }
+  // TODO: Begin your code here. Feel free to add any helper functions or classes you need,
+  // as long as we only have to call this function to run the specified assignment.
+  string FILENAME = argv[1]; // filename is argv[1] string 
+  ifstream FILE(FILENAME);
+  if(!FILE) { // if file is bad abort 
+    cout<<"BAD FILE!";
+    return 0;
+  }
     
-    int num_vertices;
-    FILE>>num_vertices; // read num of vertices 
+  int num_vertices;
+  FILE>>num_vertices; // read num of vertices 
     
-    Graph G(num_vertices); // create a graph with a constructor of num_vertices
+  Graph G(num_vertices); // create a graph with a constructor of num_vertices
 
+  FILE.ignore(1000,'\n');    // ignore \n because we will be reading strings 
 
-  
-         FILE.ignore(1000,'\n');    // ignore \n because we will be reading strings 
-
-    while(!FILE.eof()) { // loop as long as file is not at the end 
-          string help;    // new string 
-     getline(FILE,help); // read a line 
+  while(!FILE.eof()) { // loop as long as file is not at the end 
+    string help;    // new string 
+    getline(FILE,help); // read a line 
      
-     vector<string> V; // create a vector of strings 
-     for(int i=0;i<help.length();i++) { // go through a vector 
+    vector<string> V; // create a vector of strings 
+    for(int i=0;i<help.length();i++) { // go through a vector 
          
-         string s;
-         while(help[i] != ' ' && i < help.length()) { // as long as it is not whitespace or end 
+        string s;
+        while(help[i] != ' ' && i < help.length()) { // as long as it is not whitespace or end 
              
-             s.push_back(help[i]); // add it to a string 
+            s.push_back(help[i]); // add it to a string 
              
              
-             i++;
-         }
+            i++;
+        }
          
-         V.push_back(s); // string in the end is a number as a string so we push it back to V 
+        V.push_back(s); // string in the end is a number as a string so we push it back to V 
 
-     }
+    }
 
-         int num = stoi(V[0]); // convert first number from a string to int 
+        int num = stoi(V[0]); // convert first number from a string to int 
        
-         for(int i=1;i<V.size();i++) { // go from 1 to the end of vector of strings 
+        for(int i=1;i<V.size();i++) { // go from 1 to the end of vector of strings 
              
-             int vert1 = stoi(V[i]); // convert incoming number from a string to number 
-             i++;
-             double weight1 = stod(V[i]); //convert weight to a double 
-             Edge E(vert1,weight1); // create edge 
+            int vert1 = stoi(V[i]); // convert incoming number from a string to number 
+            i++;
+            double weight1 = stod(V[i]); //convert weight to a double 
+            Edge E(vert1,weight1); // create edge 
              
-           G.insert(num,E); // insert to a graph 
+          G.insert(num,E); // insert to a graph 
              
                  // insert it together with the vertex
              
-         }
+        }
         
-    }
+  }
     
     
-    FILE.close();
-    string FILENAME2 = argv[2]; // a new file 
-    ifstream FILE2(FILENAME2); // read a file2 
+  FILE.close();
+  string FILENAME2 = argv[2]; // a new file 
+  ifstream FILE2(FILENAME2); // read a file2 
 
-    if(!FILE2) {
-        cout<<"COULD NOT OPEN FILE2";
-        return 0;
-    }
+  if(!FILE2) {
+      cout<<"COULD NOT OPEN FILE2";
+      return 0;
+  }
     
-    while(!FILE2.eof()) {
+  while(!FILE2.eof()) {
         
         int num1;
         int num2;
